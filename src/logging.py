bcffdf11355bb55901ed8933f5de675fedfae7c9
davidserra9 <99d.serrano@gmail.com>
2023-06-15 16:01:41 +0200

    custom logging

--- src/logging.py
+++ src/logging.py
@@ -1,3 +1,5 @@
+import os
+import time
 import logging
 import sys
 
@@ -27,9 +29,31 @@ def set_logging():
     logger = logging.getLogger()
     logger.setLevel(logging.DEBUG)
 
+    # Create the root folder if it does not exist
+    root_folder = os.path.join(os.getcwd(), "logs")
+    if not os.path.exists(root_folder):
+        os.makedirs(root_folder)
+
+    # Create subfolder for the current run
+    timestamp = time.strftime("%Y-%m-%d_%H-%M-%S", time.localtime())
+    log_folder = os.path.join(root_folder, timestamp)
+    os.makedirs(log_folder)
+
+    # Set up the log file path
+    log_file = os.path.join(log_folder, "log.log")
+
+    # Configure the file handler with the log file path
+    fh = logging.FileHandler(log_file)
+    fh.setLevel(logging.DEBUG)
+    fh.setFormatter(CustomFormatter())
+
+    # Configure the console handler
     ch = logging.StreamHandler(sys.stdout)
     ch.setLevel(logging.INFO)
     ch.setFormatter(CustomFormatter())
+
+    # Add both handlers to the logger
+    logger.addHandler(fh)
     logger.addHandler(ch)
 
     return logger
